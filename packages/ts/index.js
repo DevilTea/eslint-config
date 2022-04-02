2c200bcf80c5d9408b5cefa618eec5b5d03a98df
DevilTea <ch870814@gmail.com>
2022-04-02 23:58:09 +0800

    fix(ts): update naming-convention rule

--- packages/ts/index.js
+++ packages/ts/index.js
@@ -38,6 +38,31 @@ module.exports = {
 
     'space-before-function-paren': 'off',
     '@typescript-eslint/space-before-function-paren': ['error', 'always'],
+
+    '@typescript-eslint/naming-convention': [
+      'error',
+      // Allow camelCase variables (23.2), PascalCase variables (23.8), and UPPER_CASE variables (23.10)
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+      // Allow camelCase functions (23.2), and PascalCase functions (23.8)
+      {
+        selector: 'function',
+        format: ['camelCase', 'PascalCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+      // Airbnb recommends PascalCase for classes (23.3), and although Airbnb does not make TypeScript recommendations, we are assuming this rule would similarly apply to anything "type like", including interfaces, type aliases, and enums
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+    ],
   },
   overrides: base.overrides,
 }
