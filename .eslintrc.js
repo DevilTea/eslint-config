aa11a8375d7f373e4c9facdd29efbac41123b0ec
DevilTea <ch870814@gmail.com>
2022-10-30 23:35:52 +0800

    feat: update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,25 @@ module.exports = {
     // vue
     'vue/component-tags-order': [
       'error',
-      { order: ['template', 'script[setup]', 'script:not([setup])', 'style'] },
+      {
+        order: [
+          'template',
+          'script:not([setup])',
+          'script[setup]',
+          'style:not([scoped])',
+          'style[scoped]',
+        ],
+      },
     ],
     // js / ts
     '@typescript-eslint/brace-style': ['error', '1tbs', { allowSingleLine: true }],
     '@typescript-eslint/space-before-function-paren': ['error', 'always'],
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', ignoreRestSiblings: true }],
     'arrow-parens': ['error', 'always'],
+    'sort-imports': ['off'],
+    // import
+    'import/first': ['off'],
+    'import/no-duplicates': ['off'],
   },
   overrides: [
     {
